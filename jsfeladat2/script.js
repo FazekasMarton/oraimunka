cf5980fdc7ffe8e841d72a69c59d6f9c853c2120
FazekasMarton <fazekas.marton16@gmail.com>
2021-04-22 08:57:00 +0200

    textcolor change

--- jsfeladat2/script.js
+++ jsfeladat2/script.js
@@ -1,11 +1,15 @@
 function colorc(){
     var a = 123;
     var color = [0, 0, 0];
+    var color2 = [0, 0, 0];
     for(i = 0; i < 3; i++){
         color[i] = Math.floor(Math.random() * 256);
+        color2[i] = 255 - color[i];
     }
     var szin = "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")";
+    var szin2 = "rgb(" + color2[0] + "," + color2[1] + "," + color2[2] + ")";
     document.getElementById("gomb").style.background = szin;
+    document.getElementById("gomb").style.color = szin2;
 }
 function hozzaad(){
     var box = document.getElementById("doboz").value;
